3ebfa7fc1c69bdc269d48b4a522c4af50bdc6e02
Simon Andrews <simon.andrews212@gmail.com>
2018-10-13 00:18:21 -0400

    Add some job listing for testing

---
+++ resgen/listings.py
@@ -0,0 +1,42 @@
+listing = """
+An exciting new opportunity has opened for a Cloud Technologist within Incident
+Change and Management Squad that is part of Virtualization Tribe in ECC
+(Enterprise Cloud Computing). The successful candidate will wear many hats:
+first responder as L1 / L2, Consulting to Business partners, Interface with many
+DevOps Squads and Tribes, FSC and App support teams. Superlative technical and
+communication skills are required for you to be successful within this role. Key
+Responsibilities Lead and develop our Infrastructure Operations Team to greater
+heights of effectiveness, customer service and accomplishment. Ensure
+Infrastructure Operations Team is adhering to Information Security, Regulatory
+Compliance, Incident Management, Problem Management and Change Management
+processes/practices. Responsible for the proper monitoring and alerting of all
+environments Responsible for internal infrastructure projects which may require
+cross-departmental coordination to achieve goals. Provide input into product
+teams on infrastructure lifecycle, improvement and standardization strategies.
+Drive the objectives associated with Problem Management; such as customer
+communication and Root Cause Analysis reports. Develop internal SOPs, processes
+and training to ensure Infrastructure team members have the needed skills and
+tools to support the production environments and deliver on project commitments.
+Work with application owners and product teams as requested to assess
+applications and recommend infrastructure/cloud solutions. Required Skills 2
+years' experience with AWS foundational cloud services for storage, computation,
+event processing, messaging, data processing, and analytics 5 years of software
+development with object-oriented languages such as Python, Java or C using SOA
+principles Strong familiarity with Linux OS (Redhat) Solid experience with JSON,
+XML and related notational data representations Solid understanding of DevOps
+principles, experience with operational tools (Ansible, Chef, Puppet, TerraForm,
+CloudFormation) and best practices for infrastructure and software deployment
+Extensive knowledge of APIs, RESTful services and integration with Cloud data
+providers Expert scripting skills in at least one of the following: bash, perl
+or python Competent in Linux systems administration Server configuration
+management knowledge Familiarity with formal risk management policies and
+procedures Familiar with scanning and penetration tools and techniques
+Application packaging knowledge Able to research, test and implement new
+software within the company's infrastructure Qualifications Must love data
+Ability to adapt quickly to new technologies and changing business requirements
+Able and willing to collaborate as part of a team in various roles Respond to
+emergency situations effectively, maintaining poise and focus Willingness to
+work after hours on project-related work and pre-scheduled on-call support AWS
+Certification is desirable Excellent communication (written and verbal) and
+interpersonal skills
+"""
