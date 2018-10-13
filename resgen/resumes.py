b4803a8b9c60b877aac04775d4e10ab1420fb62c
Simon Andrews <simon.andrews212@gmail.com>
2018-10-13 13:01:39 -0400

    Template rendering with Jinja

--- resgen/resumes.py
+++ resgen/resumes.py
@@ -1,4 +1,15 @@
+from jinja2 import Environment, FileSystemLoader
 import json
+
+env = Environment(
+    loader=FileSystemLoader('templates/resume'),
+
+    block_start_string='(@', # replaces {%
+    block_end_string='@)', # replaces %}
+    variable_start_string='((', # replaces {{
+    variable_end_string='))', # replaces }}
+)
+resume_template = env.get_template('resume.tex')
 
 class ResumeManager:
     def __init__(self, json_path):
@@ -7,8 +18,11 @@ class ResumeManager:
             self.data = json.loads(text)
     def get_sections(self):
         return list(self.data['sections'].keys())
+    def render(self):
+        return resume_template.render(data=self.data)
 
 if __name__ == '__main__':
     r = ResumeManager('sample_data/resume.json')
     import pprint
     pprint.pprint(r.get_sections())
+    print(r.render())
