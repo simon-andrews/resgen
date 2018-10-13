f3ffd59d2deda66cd800d3fcdaad916d2d6648d9
Simon Andrews <simon.andrews212@gmail.com>
2018-10-13 10:57:52 -0400

    Smoosh two similar functions into one

--- resgen/listings.py
+++ resgen/listings.py
@@ -5,19 +5,16 @@ class Listing:
     def __init__(self, text):
         self.text = text
         self.tokens = tokenization.tokenize(self.text)
-    def get_languages(self):
-        if hasattr(self, 'languages'):
-            return self.languages
         self.languages = set()
+        self.frameworks_and_tools = set()
+    # TODO: handle multi-word skills
+    def _update_skill(self, predicate, skill_set):
         for token in self.tokens:
-            if skills.is_language(token):
-                self.languages.add(token)
+            if predicate(token):
+                skill_set.add(token)
+    def get_languages(self):
+        _update_skill(skills.is_language, self.languages)
         return self.languages
     def get_frameworks_and_tools(self):
-        if hasattr(self, 'frameworks_and_tools'):
-            return self.frameworks_and_tools
-        self.frameworks_and_tools = set()
-        for token in self.tokens:
-            if skills.is_framework_or_tool(token):
-                self.frameworks_and_tools.add(token)
+        _update_skill(skills.is_framework_or_tool, self.frameworks_and_tools)
         return self.frameworks_and_tools
