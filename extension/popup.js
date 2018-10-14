ee76ba4e3933c0a8c2535343e8c9e94574cb18cb
Simon Andrews <simon.andrews212@gmail.com>
2018-10-14 03:55:02 -0400

    Add posting support

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,28 @@
+
+function post(path, params, method) {
+  method = method || "post";
+
+  var form = document.createElement("form");
+  form.setAttribute("method", method);
+  form.setAttribute("action", path);
+
+  for(var key in params) {
+    if(params.hasOwnProperty(key)) {
+      var hiddenField = document.createElement("input");
+      hiddenField.setAttribute("type", "hidden");
+      hiddenField.setAttribute("name", key);
+      hiddenField.setAttribute("value", params[key]);
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
+
 function doStuffWithDom(domContent) {
-  console.log('I received the following DOM content: ' + domContent);
+  post('http://127.0.0.1:5000/genresume', {data: domContent});
+  console.log(domContent);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
